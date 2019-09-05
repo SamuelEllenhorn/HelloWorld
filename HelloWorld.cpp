d3fa34eb85b27914ca1e3b25043af9e7b736c118
Samuel <you@example.comsamuelellenhorn@gmail.com>
2019-09-05 23:19:59 +0000

    Adds HelloWorld.cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,17 @@
+/*
+Samuel Ellenhorn
+2295046
+Ellenhorn@chapman.edu
+Computer Science 350
+Assignment 1
+*/
+
+//Program will output HelloWorld.
+#include<iostream>
+	using namespace std;
+
+	int main()
+	{
+		cout << "Hello World!";
+		return 0;
+	}
